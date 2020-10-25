0b6ac0f28f2e9bcff9049dc45fad62468dc0a583
u1lowsogood <yuichirosogood@gmail.com>
2020-10-25 18:56:33 +0900

    Kakikae Manzi

--- DiscordBot/main.py
+++ DiscordBot/main.py
@@ -6,12 +6,8 @@ from controller import MainController
 #モデルファイル読み込み
 #from model import MainModel
 
-print("manko")
-
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
-
-print("manko")
 
 # 起動時に動作する処理
 @client.event
@@ -22,25 +18,54 @@ async def on_ready():
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
-    talk = ''
+    #talk = ''
 
     # BOTのメッセージは無視する
-    if message.author.bot:
-        return
+    #if message.author.bot:
+    #    return
 
     # その他のメッセージだった場合処理
-    else:
-        discord_id = message.author.id
+    #else:
+    #   discord_id = message.author.id
 
         #Controllerの
-        talk = MainController.MainProgram(discord_id,message.content)
+    #    talk = MainController.MainProgram(discord_id,message.content)
 
-    print('Duck：',talk)
+    #print('Duck：',talk)
 
     #DiscordBotがメッセージ送信
-    await message.channel.send(talk)
+    #await message.channel.send(talk)
+
+        # JOIN VC
+    if message.content == ('金玉燃やすぞ') or message.content == ('全召喚') :
+        vcstate = message.author.voice
+
+        if (not vcstate) or (not vcstate.channel):
+            await message.channel.send("金玉どこだよ")
+            return
+
+        voicechannel = vcstate.channel
+        await voicechannel.connect()
+        activity = discord.Activity(
+            name='【ケツ穴拡張！？ドスケベマンコRoyal】Solo - Squad 4/3 残糞：残り(一億 人) ', type=discord.ActivityType.watching)
+        await client.change_presence(activity=activity)
+        await message.channel.send('うるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさい')
+
+    # LEAVE VC
+    if message.content == ('金玉叩き割ったわ'):
+        vcclient = message.guild.voice_client
+
+        if not vcclient:
+            await message.channel.send("うるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさい")
+            return
+
+        await vcclient.disconnect()
+        await message.channel.send('うるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさい')
 
 
-# Botの起動とDiscordサーバーへの接続
-client.run('NzY5ODQxMjg4NTAwNjA5MDQ0.X5U4kg.s46Ul_ayfheAuXKmt0YbXXkdFdw')
-
+# ゆういちのローカルファイルからトークンを取得
+f = open('D:\\Git\\botoken.txt', 'r', encoding='UTF-8')
+token = f.read()
+print(token)
+client.run(token)
+f.close()
