189593254ab0c772592789b7d61b032eb9c1395a
miyokz <miyokawa@outlook.com>
2020-10-26 21:30:32 +0900

    Controllerにわけた

--- DiscordBot/controller/MainController.py
+++ DiscordBot/controller/MainController.py
@@ -3,5 +3,32 @@
 #まいんこんとろーら
 
 def MainProgram(discord_id, message):
-	return str(discord_id) + message
 
+	    # JOIN VC
+    if message.content == ('金玉燃やすぞ') or message.content == ('全召喚') or message.content == ('tamago'):
+        vcstate = message.author.voice
+
+        if (not vcstate) or (not vcstate.channel):
+            return "金玉どこだよ"
+
+        voicechannel = vcstate.channel
+
+        voicechannel.connect()
+        activity = discord.Activity(
+            name='【ケツ穴拡張！？ドスケベマンコRoyal】Solo - Squad 4/3 残糞：残り(一億 人) ', type=discord.ActivityType.watching)
+
+        client.change_presence(activity=activity)
+
+        return 'うるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさい'
+
+    # LEAVE VC
+    if message.content == ('金玉叩き割ったわ'):
+        vcclient = message.guild.voice_client
+
+        if not vcclient:
+            return "うるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさい"
+
+        vcclient.disconnect()
+        return 'うるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさい'
+
+    return ''
--- DiscordBot/main.py
+++ DiscordBot/main.py
@@ -18,50 +18,25 @@ async def on_ready():
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
-    #talk = ''
+    talk = ''
 
     # BOTのメッセージは無視する
-    #if message.author.bot:
-    #    return
+    if message.author.bot:
+        return
 
     # その他のメッセージだった場合処理
-    #else:
-    #   discord_id = message.author.id
+    else:
+       discord_id = message.author.id
 
-        #Controllerの
-    #    talk = MainController.MainProgram(discord_id,message.content)
-
-    #print('Duck：',talk)
+    #Controllerの
+    talk = MainController.MainProgram(discord_id, message)
 
     #DiscordBotがメッセージ送信
-    #await message.channel.send(talk)
+    if talk != (''):
+        print('Duck：',talk)
+        await message.channel.send(talk)
 
-        # JOIN VC
-    if message.content == ('金玉燃やすぞ') or message.content == ('全召喚') or message.content == ('tamago'):
-        vcstate = message.author.voice
-
-        if (not vcstate) or (not vcstate.channel):
-            await message.channel.send("金玉どこだよ")
-            return
-
-        voicechannel = vcstate.channel
-        await voicechannel.connect()
-        activity = discord.Activity(
-            name='【ケツ穴拡張！？ドスケベマンコRoyal】Solo - Squad 4/3 残糞：残り(一億 人) ', type=discord.ActivityType.watching)
-        await client.change_presence(activity=activity)
-        await message.channel.send('うるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさい')
-
-    # LEAVE VC
-    if message.content == ('金玉叩き割ったわ'):
-        vcclient = message.guild.voice_client
-
-        if not vcclient:
-            await message.channel.send("うるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさい")
-            return
-
-        await vcclient.disconnect()
-        await message.channel.send('うるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさい')
-
+    
 
 # ゆういちのローカルファイルからトークンを取得
 f = open('D:\\Git\\botoken.txt', 'r', encoding='UTF-8')
