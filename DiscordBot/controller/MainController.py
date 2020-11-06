a526a39a4add608d8e0e5a19734d4f67f5faa4f8
miyokz <miyokawa@outlook.com>
2020-11-06 21:14:05 +0900

    AOKIカウントダウン追加

--- DiscordBot/controller/MainController.py
+++ DiscordBot/controller/MainController.py
@@ -1,10 +1,11 @@
 #import なんかインポートして
+import datetime
 
 #まいんこんとろーら
 
 def MainProgram(discord_id, message):
 
-	    # JOIN VC
+        # JOIN VC
     if message.content == ('金玉燃やすぞ') or message.content == ('全召喚') or message.content == ('tamago'):
         vcstate = message.author.voice
 
@@ -30,5 +31,16 @@ def MainProgram(discord_id, message):
 
         vcclient.disconnect()
         return 'うるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさいうるさい'
+
+    if message.content == ('atAoki'):
+        jstart_dt = datetime.datetime.now()
+        jend_dt = datetime.datetime(year=2021, month=2, day=16, hour=14)
+
+        jat_time = jend_dt - jstart_dt
+        a_m, a_s = divmod(jat_time.seconds, 60)
+        a_h, a_m = divmod(a_m, 60)
+
+        r_jat_time = 'あと' + str(jat_time.days) + '日' + str(a_h) + '時間' + str(a_m) + '分' + str(a_s) + '秒' + str(jat_time.microseconds)
+        return r_jat_time
 
     return ''
